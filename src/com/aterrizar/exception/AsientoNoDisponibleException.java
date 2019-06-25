fb70cdb0b127ac516f60e5217f15d954c037bbd9
goncxjo <gonzalo.moreira@live.com.ar>
2019-06-25 20:04:01 -0300

    fix excepción

--- src/com/aterrizar/exception/AsientoNoDisponibleException.java
+++ src/com/aterrizar/exception/AsientoNoDisponibleException.java
@@ -2,5 +2,6 @@ package com.aterrizar.exception;
 
 public class AsientoNoDisponibleException extends Exception {
     public AsientoNoDisponibleException(String message) {
+        super(message);
     }
 }
